ccb1836a7927d64a6211d2c527a57470ee7737ee
Wayne Robert <wayne.robert@capgemini.com>
2022-11-28 11:28:45 +0100

    INitial setup for the 1st

---
+++ AdventOfCode/src/main/java/nl/wayne/j/robert/advent/of/code/util/FileReader.java
@@ -0,0 +1,61 @@
+package nl.wayne.j.robert.advent.of.code.util;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileReader {
+
+    public FileReader() {
+    }
+
+    public List<Integer> readIntInput(String filePath) {
+        List<Integer> fileLines = new ArrayList<>();
+        try {
+            Path path = Paths.get(getClass().getClassLoader().getResource(filePath).toURI());
+            Stream<String> lines = Files.lines(path);
+            fileLines = lines
+                    .map(Integer::valueOf)
+                    .collect(Collectors.toList());
+            lines.close();
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return fileLines;
+    }
+
+    public List<Long> readLongInput(String filePath) {
+        List<Long> fileLines = new ArrayList<>();
+        try {
+            Path path = Paths.get(getClass().getClassLoader().getResource(filePath).toURI());
+            Stream<String> lines = Files.lines(path);
+            fileLines = lines
+                    .map(Long::valueOf)
+                    .collect(Collectors.toList());
+            lines.close();
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return fileLines;
+    }
+
+    public List<String> readInput(String filePath) {
+        List<String> fileLines = new ArrayList<>();
+        try {
+            Path path = Paths.get(getClass().getClassLoader().getResource(filePath).toURI());
+            Stream<String> lines = Files.lines(path);
+            fileLines = lines
+                    .collect(Collectors.toList());
+            lines.close();
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return fileLines;
+    }
+}
