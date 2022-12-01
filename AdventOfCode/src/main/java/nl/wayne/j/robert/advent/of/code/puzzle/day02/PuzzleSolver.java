bf0b8482be334e21b9a7daf22ec3e597cb333ed0
Wayne Robert <wayne.robert@capgemini.com>
2022-12-01 08:39:51 +0100

    Day 2 setup

---
+++ AdventOfCode/src/main/java/nl/wayne/j/robert/advent/of/code/puzzle/day02/PuzzleSolver.java
@@ -0,0 +1,24 @@
+package nl.wayne.j.robert.advent.of.code.puzzle.day02;
+
+import nl.wayne.j.robert.advent.of.code.util.FileReader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PuzzleSolver {
+
+    private FileReader adventFileReader;
+
+    public PuzzleSolver() {
+        this.adventFileReader = new FileReader();
+    }
+
+    public int solvePuzzleOne(String inputFilePath) {
+        return 0;
+    }
+
+    public int solvePuzzleTwo(String inputFilePath) {
+        return 0;
+    }
+}
---
+++ AdventOfCode/src/test/java/nl/wayne/j/robert/advent/of/code/puzzle/day02/PuzzleSolverTest.java
@@ -0,0 +1,22 @@
+package nl.wayne.j.robert.advent.of.code.puzzle.day02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PuzzleSolverTest {
+
+    @Test
+    void testSolvePuzzleOne() {
+        PuzzleSolver puzzleSolver = new PuzzleSolver();
+
+        assertEquals(0, puzzleSolver.solvePuzzleOne("day02/puzzle_input.txt"));
+    }
+
+    @Test
+    void testSolvePuzzleTwo() {
+        PuzzleSolver puzzleSolver = new PuzzleSolver();
+
+        assertEquals(0, puzzleSolver.solvePuzzleTwo("day02/puzzle_input.txt"));
+    }
+}
